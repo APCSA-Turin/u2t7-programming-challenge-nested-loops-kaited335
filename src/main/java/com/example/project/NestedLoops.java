744a63ff6d956f7571bea6b414ea1336b3bd75fc
Katie Choy <katiec63@nycstudents.net>
2025-12-10 18:15:10 +0000

    .

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,7 +17,15 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String stars = "";
+    for (int row = 1; row <= height; row++) {
+        for (int col = 1; col <= row; col ++) {
+            stars += "*";
+        }
+        stars += "\n";
+    }
+    stars = stars.substring(0, stars.length()-1);
+    return stars;
 }
           /*
 return the string below for given height=5
@@ -28,8 +36,16 @@ return the string below for given height=5
 *
            */
 
-public static String starStaircaseReverse(int height){
-    return "";
+public static String starStaircaseReverse(int height){ 
+    String stars = "";
+    for (int row = height; row >= 1; row--) {
+        for (int col = row; col >= 1; col --) {
+            stars += "*";
+        }
+        stars += "\n";
+    }
+    stars = stars.substring(0, stars.length()-1);
+    return stars;
 }
             /*
 
@@ -41,7 +57,24 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+        if (width == 1 || height == 1) {
+            return "";
+        }
+        String stars = "";
+        for (int row = 1; row <= height; row ++) {
+            for (int col = 1; col <= width; col ++) {
+                if (row == 1 || row == height) {
+                    stars += "*";
+                } else if (col == 1 || col == width) {
+                    stars += "*";
+                } else {
+                    stars += " ";
+                }
+            }
+            stars += "\n";
+        }
+        stars = stars.substring(0, stars.length()-1);
+        return stars;
     }
                     /*
 return the string below for word="HELLO" rows=5                    
@@ -54,7 +87,19 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
+        String answer = "";
+        int split = 0;
+        for (int i = 0; i < rows; i++) {
+            split = i % word.length();
+            if (split == 0) {
+                answer += word;
+            } else {
+                answer += word.substring(split) + word.substring(0, split);
+            }
+            answer += "\n";
+        }
+        answer = answer.substring(0, answer.length() - 1);
+        return answer;
     }
 
 }
--- src/main/java/com/example/project/NestedLoopsRunner.java
+++ src/main/java/com/example/project/NestedLoopsRunner.java
@@ -2,7 +2,8 @@ package com.example.project;
 
 public class NestedLoopsRunner {
     public static void main(String[] args) {
-     
+        NestedLoops loop = new NestedLoops();
+        System.out.println(loop.repeatRectangle("HELLO", 9));
     }
 }
 
